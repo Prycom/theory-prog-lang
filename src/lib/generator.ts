822ada12ede3cc802b9b8fd2a5d252349425367b
Prycom <as.gorohov2003@gmail.com>
2024-09-23 05:23:11 +0700

    created first ex

---
+++ src/lib/generator.ts
@@ -0,0 +1,68 @@
+type GrammarRule = {
+    lhs: string;
+    rhs: string[];
+}
+
+type Grammar = {
+    startSymbol: string;
+    rules: Map<string, string[]>;
+    terminalSymbols: string[];
+}
+
+export function replaceInv(str: string, searchValue: string, replaceValue: string): string {
+	const lastIndex = str.lastIndexOf(searchValue);
+	if (lastIndex === -1) return str;
+	return str.substring(0, lastIndex) + replaceValue + str.substring(lastIndex + searchValue.length);
+}
+
+
+
+export function leftToRight(grammar: Grammar, minLen: number, maxLen: number, reversed: boolean = false): string[] {
+    const chains: string[] = [];
+    const chainsProgress: string[] = [];
+
+    const nonTerminalSymbols = Array.from(grammar.rules.keys());
+
+    function gen(currChain: string, currProgress: string) {
+        const count = Array.from(currChain).filter(ch => !nonTerminalSymbols.includes(ch)).length;
+
+        if (count > maxLen) {
+            console.log(currChain, currChain.length)
+            return;
+        }
+        for (const [nonTerminal, terminalSymbols] of grammar.rules) {
+            for (const terminal of terminalSymbols) {
+                
+                if(currChain.includes(nonTerminal)){ // 
+                    //console.log(`Current chain: ${currChain}\nNon terminal: ${nonTerminal}\nReplaced with: ${terminal}`)
+                    let newChain = ''
+                    if(reversed){
+                        newChain = replaceInv(currChain, nonTerminal, terminal)
+                    }else{
+                        newChain = currChain.replace(nonTerminal, terminal)
+                    }
+                    
+                    const newProgress = currProgress + " => " + newChain;
+                    
+                    if (newChain.length >= minLen && newChain.length <= maxLen
+                        && !nonTerminalSymbols.some(nonTermSymb => newChain.includes(nonTermSymb))) {
+                        // if no non terminal symbols and normal size of string    
+                        chains.push(newChain)
+                        chainsProgress.push(newProgress);
+                    }
+    
+                    gen(newChain, newProgress);
+    
+                }
+                
+
+            }
+
+        }
+    }
+    gen(grammar.startSymbol, grammar.startSymbol);
+
+    console.log(chainsProgress)
+
+    return chains.concat(chainsProgress);
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,121 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { leftToRight, replaceInv } from '$lib/generator';
+
+	let grammar = {
+		startSymbol: 'S',
+		rules: new Map([]),
+			/*
+        new Map([
+			['S', ['aA', 'bB']],
+			['A', ['aS', '']],
+			['B', ['bB', '']]
+		])
+        */
+		terminalSymbols: []
+	};
+
+	let minLen = 1;
+	let maxLen = 5;
+	let generatedChains: string[] = [];
+
+	let ruleName = '';
+	let ruleTerminal = '';
+    let newTermSym = '';
+
+	function generateChains() {
+		generatedChains = leftToRight(grammar, minLen, maxLen, true);
+	}
+
+	function addRule() {
+		const key = ruleName.trim();
+		const values = ruleTerminal.trim().split('|');
+
+		if (key && values.length > 0) {
+			grammar = {
+				...grammar,
+				rules: new Map(grammar.rules.set(key, values))
+			};
+		}
+
+        ruleName = ''
+        ruleTerminal = ''
+	}
+
+    function addTermSymb(){
+        const term = newTermSym.trim()
+        const terms = grammar.terminalSymbols;
+        if(term.length > 0){
+            grammar = {
+                ...grammar,
+                terminalSymbols: [...grammar.terminalSymbols, term]
+            }
+        }
+        newTermSym = ''
+    }
+
+</script>
+
+<div class="flex w-full h-24 bg-slate-300">
+	<h1 class="block m-auto text-2xl font-bold">
+		Бригада #3: Бойко<span class="text-yellow-500">⚡⚡</span>,
+		<span class="text-red-500">Горохов❤️</span>
+	</h1>
+</div>
+
+<h2 class="text-xl font-semibold my-6">Лабораторная #1</h2>
+
+<button class="italic my-4 mx-4" on:click={() => console.log(grammar)}>[!] Debug INFO</button>
+<h3 class="text-md font-semibold">Grammar</h3>
+<div>
+	<label for="start_sym">Start Symbol: </label>
+	<input id="start_sym" class="border-2" bind:value={grammar.startSymbol} />
+</div>
+<h3 class="font-semibold">Rules:</h3>
+<button class="border-2 bg-gray-300" on:click={addRule}>Add new rule</button>
+
+<input placeholder="Rule name A" bind:value={ruleName} />
+<input placeholder="Rule aA|...." bind:value={ruleTerminal} />
+
+<div class='border-2 p-4'>
+	{#each grammar.rules as [nonTerminalSyms, terminalSyms]}
+		<span class="block border-b-2 mx-2">{nonTerminalSyms}</span>
+		{#each terminalSyms as termSym}
+			<span class="block mx-4">{termSym}</span>
+		{/each}
+	{/each}
+</div>
+
+
+
+
+<h3 class="font-semibold">Terminal Symbols:</h3>
+
+<button class="border-2 bg-gray-300" on:click={addTermSymb}>Add new terminal symbol</button>
+<input placeholder="Your terminal symbol" bind:value={newTermSym} />
+<div>
+    {#each grammar.terminalSymbols as termSymb}
+        <span class='block border-b-2 mx-2'>{termSymb}</span>
+    {/each}
+</div>
+
+<form on:submit|preventDefault={generateChains} class="mb-4">
+	<div class="m-4">
+		<label for="minLength">Min Length:</label>
+		<input type="number" id="minLength" bind:value={minLen} />
+
+		<label for="maxLength">Max Length:</label>
+		<input type="number" id="maxLength" bind:value={maxLen} />
+
+		<button type="submit" class="border-gray-400 border-2">Generate Chains</button>
+	</div>
+</form>
+
+{#if generatedChains.length > 0}
+	<h2>Generated Chains:</h2>
+	<ul class="">
+		{#each generatedChains as chain}
+			<li class="border-b-2">{chain}</li>
+		{/each}
+	</ul>
+{/if}
